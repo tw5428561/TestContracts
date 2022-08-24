b30ef37e0cffa9c143101a35ab49bfe210f78e28
jason <jason.tian@davionlabs.com>
2022-08-24 16:10:02 +0800

    add op721 task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,10 +11,19 @@ import "./tasks/ERC20.task"
 import "./tasks/deployAll.task"
 import "./tasks/ERC721.task"
 import "./tasks/ERC1155.task"
+import "./tasks/op_erc721.task"
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.11",
+  solidity:{
+  compilers: [
+    {
+      version: "0.7.6",
+    },
+    {
+      version: "0.8.11",
+    }]
+    },
   networks: {
     hardhat: {
       forking: {
@@ -44,7 +53,28 @@ const config: HardhatUserConfig = {
       gas: 4100000,
       accounts:['7eefd641410560e690736ee331bd32512c9b58419a877eff2189facbef33cd1e']
     },
-
+    opl2: { // Optimism Goerli
+      url: "https://goerli.optimism.io",
+      accounts: {
+        mnemonic:'enforce image nasty ahead clutch muscle foil broom thought shoot bless critic'
+      },
+      // accounts:['0x7eefd641410560e690736ee331bd32512c9b58419a877eff2189facbef33cd1e'],
+      chainId: 420,
+      gas: 10000000,
+      gasPrice: 5000000,
+      //explorer: https://blockscout.com/optimism/goerli/
+    },
+    opl1: { // Optimism Goerli
+      url: "https://rpc.ankr.com/eth_goerli",
+      accounts: {
+        mnemonic:'enforce image nasty ahead clutch muscle foil broom thought shoot bless critic'
+      },
+      //accounts:['0x7eefd641410560e690736ee331bd32512c9b58419a877eff2189facbef33cd1e'],
+      chainId: 5,
+      gas: 10000000,
+      gasPrice: 5000000,
+      //explorer: https://blockscout.com/optimism/goerli/
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
